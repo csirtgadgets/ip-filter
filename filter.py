752fe4af60292519d12767f2a2df751aa373e48d
wes <wesyoung@users.noreply.github.com>
2019-08-27 19:19:43 -0400

    Create filter.py

---
+++ filter.py
@@ -0,0 +1,28 @@
+
+# pip install pytricia
+
+import pytricia
+
+PERM_WHITELIST = [
+    "0.0.0.0/8",
+    "10.0.0.0/8",
+    "127.0.0.0/8",
+    "192.168.0.0/16",
+    "169.254.0.0/16",
+    "192.0.2.0/24",
+    "224.0.0.0/4",
+    "240.0.0.0/5",
+    "248.0.0.0/5",
+]
+
+# https://github.com/jsommers/pytricia
+def process(data=[], whitelist=[]):
+    wl = pytricia.PyTricia()
+    for x in PERM_WHITELIST:
+        wl[x] = True
+        
+    [wl[i] = True for i in whitelist]
+
+    for i in data:
+        if i not in wl:
+            yield i
